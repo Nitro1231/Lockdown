ffe0af835f13e9eb153abcfcbb2d2a4f37fb6ee5
Nitro <nitro0@naver.com>
2021-07-05 01:50:04 +0900

    Update UI

--- Lockdown/Lockdown/Main.Designer.cs
+++ Lockdown/Lockdown/Main.Designer.cs
@@ -24,93 +24,158 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
-            this.pwBox = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.PassBoxIndicator = new System.Windows.Forms.Label();
+            this.PassIndicator = new System.Windows.Forms.Panel();
+            this.PassPanel = new System.Windows.Forms.Panel();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.UserText = new System.Windows.Forms.Label();
+            this.ProfilePic = new System.Windows.Forms.PictureBox();
+            this.MainPanel.SuspendLayout();
+            this.PassPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfilePic)).BeginInit();
             this.SuspendLayout();
             // 
-            // pwBox
+            // MainTimer
             // 
-            this.pwBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.pwBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.pwBox.Font = new System.Drawing.Font("Segoe UI Symbol", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwBox.ForeColor = System.Drawing.Color.White;
-            this.pwBox.Location = new System.Drawing.Point(26, 12);
-            this.pwBox.Multiline = true;
-            this.pwBox.Name = "pwBox";
-            this.pwBox.PasswordChar = '*';
-            this.pwBox.Size = new System.Drawing.Size(374, 45);
-            this.pwBox.TabIndex = 0;
-            this.pwBox.WordWrap = false;
-            this.pwBox.TextChanged += new System.EventHandler(this.PwBox_TextChanged);
+            this.MainTimer.Enabled = true;
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
             // 
-            // panel1
+            // MainPanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(220)))), ((int)(((byte)(88)))));
-            this.panel1.Location = new System.Drawing.Point(26, 63);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(374, 10);
-            this.panel1.TabIndex = 1;
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.MainPanel.Controls.Add(this.PassBoxIndicator);
+            this.MainPanel.Controls.Add(this.PassIndicator);
+            this.MainPanel.Controls.Add(this.PassPanel);
+            this.MainPanel.Controls.Add(this.StatusLabel);
+            this.MainPanel.Controls.Add(this.UserText);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.MainPanel.Location = new System.Drawing.Point(0, 50);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(600, 400);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
             // 
-            // button1
+            // PassBoxIndicator
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(220)))), ((int)(((byte)(88)))));
-            this.button1.Location = new System.Drawing.Point(26, 79);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 31);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Unlock";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.PassBoxIndicator.AutoSize = true;
+            this.PassBoxIndicator.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F);
+            this.PassBoxIndicator.Location = new System.Drawing.Point(13, 403);
+            this.PassBoxIndicator.Name = "PassBoxIndicator";
+            this.PassBoxIndicator.Size = new System.Drawing.Size(0, 25);
+            this.PassBoxIndicator.TabIndex = 5;
             // 
-            // label1
+            // PassIndicator
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.label1.Location = new System.Drawing.Point(23, 113);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
+            this.PassIndicator.Location = new System.Drawing.Point(118, 236);
+            this.PassIndicator.Name = "PassIndicator";
+            this.PassIndicator.Size = new System.Drawing.Size(240, 10);
+            this.PassIndicator.TabIndex = 4;
+            this.PassIndicator.Paint += new System.Windows.Forms.PaintEventHandler(this.PassIndicator_Paint);
+            this.PassIndicator.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
             // 
-            // mainTimer
+            // PassPanel
             // 
-            this.mainTimer.Enabled = true;
-            this.mainTimer.Interval = 1000;
-            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            this.PassPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.PassPanel.Controls.Add(this.PassBox);
+            this.PassPanel.Location = new System.Drawing.Point(118, 190);
+            this.PassPanel.Name = "PassPanel";
+            this.PassPanel.Size = new System.Drawing.Size(240, 40);
+            this.PassPanel.TabIndex = 3;
+            this.PassPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            // 
+            // PassBox
+            // 
+            this.PassBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.PassBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PassBox.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassBox.ForeColor = System.Drawing.Color.White;
+            this.PassBox.Location = new System.Drawing.Point(10, 7);
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(220, 26);
+            this.PassBox.TabIndex = 2;
+            this.PassBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PassBox.UseSystemPasswordChar = true;
+            this.PassBox.TextChanged += new System.EventHandler(this.PassBox_TextChanged);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Segoe UI Variable Small", 12F);
+            this.StatusLabel.Location = new System.Drawing.Point(40, 122);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(368, 21);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "This computer is locked due to the security issue.";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StatusLabel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            // 
+            // UserText
+            // 
+            this.UserText.AutoSize = true;
+            this.UserText.Font = new System.Drawing.Font("Segoe UI Variable Display Semib", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserText.Location = new System.Drawing.Point(164, 63);
+            this.UserText.Name = "UserText";
+            this.UserText.Size = new System.Drawing.Size(144, 43);
+            this.UserText.TabIndex = 0;
+            this.UserText.Text = "UserText";
+            this.UserText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.UserText.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            // 
+            // ProfilePic
+            // 
+            this.ProfilePic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ProfilePic.Location = new System.Drawing.Point(172, 0);
+            this.ProfilePic.Name = "ProfilePic";
+            this.ProfilePic.Size = new System.Drawing.Size(100, 100);
+            this.ProfilePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ProfilePic.TabIndex = 1;
+            this.ProfilePic.TabStop = false;
+            this.ProfilePic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
-            this.ClientSize = new System.Drawing.Size(440, 135);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pwBox);
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.ProfilePic);
+            this.Controls.Add(this.MainPanel);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lockdown";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
             this.Load += new System.EventHandler(this.Main_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.PassPanel.ResumeLayout(false);
+            this.PassPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfilePic)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox pwBox;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox ProfilePic;
+        private System.Windows.Forms.Label UserText;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Panel PassPanel;
+        private System.Windows.Forms.Panel PassIndicator;
+        private System.Windows.Forms.Label PassBoxIndicator;
     }
 }
 
--- Lockdown/Lockdown/Main.cs
+++ Lockdown/Lockdown/Main.cs
@@ -1,43 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Lockdown {
     public partial class Main : Form {
-
         Lockdown lockdown;
         string pass = "1234";
 
         public Main() {
             InitializeComponent();
             lockdown = new Lockdown();
+
+            UserText.Text = $"Hello, {Environment.UserName}!";
+
+            string path = Directory.GetCurrentDirectory() + @"\ProfilePic.jpg";
+            if (File.Exists(path)) {
+                ProfilePic.Image = Image.FromFile(path);
+            }
+            TopMost = true;
         }
 
         private void Main_Load(object sender, EventArgs e) {
-            label1.Font = pwBox.Font;
+            Update();
+            Refresh();
+
+            ProfilePic.Location = new Point((Width - ProfilePic.Width) / 2, 0);
+            UserText.Location = new Point((Width - UserText.Width) / 2, ProfilePic.Height / 2 + 10);
+            StatusLabel.Location = addMargin(StatusLabel, UserText, 20);
+            PassPanel.Location = addMargin(PassPanel, StatusLabel, 50);
+
+            PassIndicator.Width = 0;
+            PassIndicator.Location = addMargin(PassIndicator, PassPanel, 5);
+
+            Utils.smoothBorder(this, 12);
+            Utils.smoothBorder(MainPanel, 12);
+            Utils.smoothBorder(ProfilePic, ProfilePic.Width);
+            Utils.smoothBorder(PassPanel, PassPanel.Height / 2);
+            Utils.smoothBorder(PassIndicator, 6);
         }
 
-        private void checkPW(string pw) {
-            if (pw.Equals(pass)) {
+        private void MainTimer_Tick(object sender, EventArgs e) {
+            lockdown.Lock();
+        }
+        
+        private void PassBox_TextChanged(object sender, EventArgs e) {
+            PassBoxIndicator.Text = new string(PassBox.PasswordChar, PassBox.TextLength);
+
+            if (PassBoxIndicator.Width >= PassBox.Width)
+                PassIndicator.Width = PassBox.Width;
+            else
+                PassIndicator.Width = PassBoxIndicator.Width;
+
+            PassIndicator.Left = (Width - PassIndicator.Width) / 2;
+            PassIndicator.Refresh();
+            Utils.smoothBorder(PassIndicator, 6);
+
+            if (PassBox.Text.Equals(pass)) {
+                MainTimer.Enabled = false;
                 lockdown.Unlock();
-                mainTimer.Enabled = false;
+                MessageBox.Show("This PC has been successfully unlocked!");
+                Close();
             }
         }
 
-        private void PwBox_TextChanged(object sender, EventArgs e) {
-            label1.Text = pwBox.Text;
-            label1.Update();
-            panel1.Width = label1.Width;
+        private void PassIndicator_Paint(object sender, PaintEventArgs e) {
+            Graphics graphics = e.Graphics;
+            Rectangle rectangle = new Rectangle(0, 0, PassIndicator.Width, PassIndicator.Height);
+            Brush brush = new LinearGradientBrush(rectangle, Color.FromArgb(40, 242, 156), Color.FromArgb(12, 184, 224), 65f);
+            graphics.FillRectangle(brush, rectangle);
         }
 
-        private void Button1_Click(object sender, EventArgs e) { checkPW(pwBox.Text); }
+        private Point addMargin(Control ctl, Control upper, int margin) {
+            return new Point((Width - ctl.Width) / 2, upper.Top + upper.Height + margin);
+        }
 
-        private void mainTimer_Tick(object sender, EventArgs e) {
-            lockdown.Lock();
+        private void Main_MouseMove(object sender, MouseEventArgs e) {
+            Utils.mouseMove(Handle);
         }
     }
 }
